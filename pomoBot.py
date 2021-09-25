3345f34565fa844b29d66a6f3a0a91ec5a40e201
csantacreu <carlossantacreuch@gmail.com>
2021-09-25 23:09:26 +0200

    first commit of the project

---
+++ pomoBot.py
@@ -0,0 +1,14 @@
+
+from datetime import date, datetime
+
+# Get the current time
+
+print(datetime.now())
+
+# Add 25 to it
+
+# When second time, send message(print)
+
+# When accepted, start 5 mnts
+
+# When 5 mnts pass, send message
